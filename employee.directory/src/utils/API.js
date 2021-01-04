ea698c8df7d0dce13eeb662163c7deaa7a869c0f
Brigitte <urbrigette@gmail.com>
2021-01-04 14:24:01 -0500

    disable-line

--- employee.directory/src/utils/API.js
+++ employee.directory/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
-
+ // eslint-disable-next-line
 export default {
   randomuser: function() {
     return axios.get(BASEURL);
